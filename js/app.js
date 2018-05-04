e47a4e696c699ac01865482ff1917cb9988d6d79
Sirapopkajaikhum <music0651@gmail.com>
2018-05-04 14:11:03 +0700

    page 1 - creta box and filter

--- js/app.js
+++ js/app.js
@@ -3,3 +3,23 @@ angular
     .controller('MainCtrl', function($scope) {
         console.log('This is a log from controller')
     })
+    .controller('FechtRow',function($http,$scope){
+       
+        $scope.products = [];
+
+        $http.get('/dataset.json').then(function(data){
+            $scope.products = data.data;
+            console.log(data)
+        })
+
+    })
+    .controller('InputTexttoFilter',function($scope){
+
+        $scope.addtext = function(textProduct){
+            $scope.textProduct.push($scope)
+        }
+
+    })
+   
+
+    
